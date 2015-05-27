b93bcfb2cfef6fa97e83988ba5450de9fdd581d0
Boris Yankov <borisyankov@gmail.com>
2015-05-27 05:05:53 +0300

    Update jquery and jquery.cookie to latest version, remove several libraries, no longer needed and code that is no longer used

--- src/javascript/binary/pages/frontend.js
+++ src/javascript/binary/pages/frontend.js
@@ -1,113 +1,3 @@
-var sidebar_scroll = function(elm_selector) {
-    elm_selector.on('click', '#sidebar-nav li', function() {
-        var clicked_li = $(this);
-        $.scrollTo($('.section:eq(' + clicked_li.index() + ')'), 500);
-        return false;
-    }).addClass('unbind_later');
-
-    if (elm_selector.size()) {
-        // grab the initial top offset of the navigation
-        var selector = elm_selector.find('.sidebar');
-        var width = selector.width();
-        var sticky_navigation_offset_top = selector.offset().top;
-        // With thanks:
-        // http://www.backslash.gr/content/blog/webdevelopment/6-navigation-menu-that-stays-on-top-with-jquery
-
-        // our function that decides weather the navigation bar should have "fixed" css position or not.
-        var sticky_navigation = function() {
-            var scroll_top = $(window).scrollTop(); // our current vertical position from the top
-
-            // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-            // otherwise change it back to relative
-            if (scroll_top > sticky_navigation_offset_top) {
-                selector.css({'position': 'fixed', 'top': 0, 'width': width});
-            } else {
-                selector.css({'position': 'relative'});
-            }
-        };
-
-        //run our function on load
-        sticky_navigation();
-
-        var sidebar_nav = selector.find('#sidebar-nav');
-        var length = elm_selector.find('.section').length;
-        $(window).on('scroll', function() {
-            // and run it again every time you scroll
-            sticky_navigation();
-
-            for (var i = 0; i < length; i++) {
-                if ($(window).scrollTop() === 0 || $(this).scrollTop() >= $('.section:eq(' + i + ')').offset().top - 5) {
-                    sidebar_nav.find('li').removeClass('selected');
-
-                    if ($(window).scrollTop() === 0) {
-                        // We're at the top of the screen, so highlight first nav item
-                        sidebar_nav.find('li:first-child').addClass('selected');
-                    } else if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
-                        // We're at bottom of screen so highlight last nav item.
-                        sidebar_nav.find('li:last-child').addClass('selected');
-                    } else {
-                        sidebar_nav.find('li:eq(' + i + ')').addClass('selected');
-                    }
-                }
-            }
-        });
-    }
-};
-
-var get_started_behaviour = function() {
-    // Get Started behaviour:
-    var update_active_subsection = function(to_show) {
-        var fragment;
-        var subsection = $('.subsection');
-        subsection.addClass('hidden');
-        to_show.removeClass('hidden');
-        var nav_back = $('.subsection-navigation .back');
-        var nav_next = $('.subsection-navigation .next');
-
-        if (to_show.hasClass('first')) {
-            nav_back.addClass('disabled');
-            nav_next.removeClass('disabled');
-        } else if (to_show.hasClass('last')) {
-            nav_back.removeClass('disabled');
-            nav_next.addClass('disabled');
-        } else {
-            nav_back.removeClass('disabled');
-            nav_next.removeClass('disabled');
-        }
-
-        fragment = to_show.find('a[name]').attr('name').slice(0, -8);
-        document.location.hash = fragment;
-
-        return false;
-    };
-    var to_show;
-    var nav = $('.get-started').find('.subsection-navigation');
-    var fragment;
-    var len = nav.length;
-
-    if (len) {
-        nav.on('click', 'a', function() {
-            var button = $(this);
-            if (button.hasClass('disabled')) {
-                return false;
-            }
-            var now_showing = $('.subsection:not(.hidden)');
-            var show = button.hasClass('next') ? now_showing.next('.subsection') : now_showing.prev('.subsection');
-            return update_active_subsection(show);
-        });
-
-        fragment = (location.href.split('#'))[1];
-        to_show = fragment ? $('a[name=' + fragment + '-section]').parent('.subsection') : $('.subsection.first');
-        update_active_subsection(to_show);
-    }
-
-    var random_market = $('.random-markets');
-    if (random_market.length > 0) {
-        sidebar_scroll(random_market);
-    }
-};
-
-
 var get_ticker = function() {
     var ticker = $('#hometicker');
     if (ticker.size()) {
@@ -263,20 +153,6 @@ var show_live_chat_icon = function() {
     }, 80);
 };
 
-var display_career_email = function() {
-    $("#hr_contact_eaddress").html(email_rot13("<n uers=\"znvygb:ue@ovanel.pbz\" ery=\"absbyybj\">ue@ovanel.pbz</n>"));
-};
-
-pjax_config_page('/$|/home', function() {
-    return {
-        onLoad: function() {
-            select_user_country();
-            get_ticker();
-            home_bomoverlay.init();
-        }
-    };
-});
-
 pjax_config_page('/contact', function() {
     return {
         onLoad: function() {
@@ -289,7 +165,8 @@ pjax_config_page('/contact', function() {
 pjax_config_page('/careers', function() {
     return {
         onLoad: function() {
-            display_career_email();
+            $("#hr_contact_eaddress")
+                .html(email_rot13("<n uers=\"znvygb:ue@ovanel.pbz\" ery=\"absbyybj\">ue@ovanel.pbz</n>"));
         },
     };
 });
