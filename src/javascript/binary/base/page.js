994685ab612a9829f6d2b5a6aa0dd6e8118c0ed0
Boris Yankov <borisyankov@gmail.com>
2015-05-27 12:31:07 +0300

    Implement pickadate, localize it, huge cleanup of unneeded js code

--- src/javascript/binary/base/page.js
+++ src/javascript/binary/base/page.js
@@ -220,142 +220,6 @@ URL.prototype = {
     },
 };
 
-var Menu = function(url) {
-    this.page_url = url;
-    var that = this;
-    $(this.page_url).on('change', function() { that.activate(); });
-};
-
-Menu.prototype = {
-    on_unload: function() {
-        this.reset();
-    },
-    activate: function() {
-        $('#menu-top li').removeClass('active');
-        this.hide_main_menu();
-
-        var active = this.active_menu_top();
-        var trading = $('#menu-top li:eq(3)');
-        if(active) {
-            active.addClass('active');
-            if(trading.is(active)) {
-                this.show_main_menu();
-            }
-        } else {
-            var is_mojo_page = /^\/$|\/login|\/home|\/smart-indices|\/ad|\/open-source-projects|\/white-labels|\/partnerapi$/.test(window.location.pathname);
-            if(!is_mojo_page) {
-                trading.addClass('active');
-                this.show_main_menu();
-            }
-        }
-    },
-    show_main_menu: function() {
-        $("#main-menu").removeClass('hidden');
-        this.activate_main_menu();
-    },
-    hide_main_menu: function() {
-        $("#main-menu").addClass('hidden');
-    },
-    activate_main_menu: function() {
-        //First unset everything.
-        $("#main-menu li.item").removeClass('active');
-        $("#main-menu li.item").removeClass('hover');
-        $("#main-menu li.sub_item a").removeClass('a-active');
-
-        var active = this.active_main_menu();
-        if(active.subitem) {
-            active.subitem.addClass('a-active');
-        }
-
-        if(active.item) {
-            active.item.addClass('active');
-            active.item.addClass('hover');
-        }
-
-        this.on_mouse_hover(active.item);
-    },
-    reset: function() {
-        $("#main-menu .item").unbind();
-        $("#main-menu").unbind();
-    },
-    on_mouse_hover: function(active_item) {
-        $("#main-menu .item").on( 'mouseenter', function() {
-            $("#main-menu li.item").removeClass('hover');
-            $(this).addClass('hover');
-        });
-
-        $("#main-menu").on('mouseleave', function() {
-            $("#main-menu li.item").removeClass('hover');
-            if(active_item)
-                active_item.addClass('hover');
-        });
-    },
-    active_menu_top: function() {
-        var active;
-        var path = window.location.pathname;
-        $('#menu-top li a').each(function() {
-            if(path.indexOf(this.pathname) >= 0) {
-                active = $(this).closest('li');
-            }
-        });
-
-        return active;
-    },
-    active_main_menu: function() {
-        var path = window.location.pathname;
-        path = path.replace(/\/$/, "");
-        path = decodeURIComponent(path);
-
-        var item;
-        var subitem;
-
-        var that = this;
-        //Is something selected in main items list
-        $("#main-menu .items a").each(function () {
-            var url = new URL($(this).attr('href'));
-            if(url.is_in(that.page_url)) {
-                item = $(this).closest('.item');
-            }
-        });
-
-        $("#main-menu .sub_items a").each(function(){
-            var url = new URL($(this).attr('href'));
-            if(url.is_in(that.page_url)) {
-                item = $(this).closest('.item');
-                subitem = $(this);
-            }
-        });
-
-        return { item: item, subitem: subitem };
-    },
-    register_dynamic_links: function() {
-        var stored_market = page.url.param('market') || LocalStore.get('bet_page.market');
-        var start_trading = $('#topMenuStartBetting a:first');
-        var trade_url = start_trading.attr("href");
-        if(stored_market) {
-            if(/market=/.test(trade_url)) {
-                trade_url = trade_url.replace(/market=\w+/, 'market=' + stored_market);
-            } else {
-                trade_url += '&market=' + stored_market;
-            }
-            start_trading.attr("href", trade_url);
-
-            $('#menu-top li:eq(3) a').attr('href', trade_url);
-        }
-
-        start_trading.on('click', function(event) {
-            event.preventDefault();
-            load_with_pjax(trade_url);
-        }).addClass('unbind_later');
-
-        $('#menu-top li:eq(3) a').on('click', function(event) {
-            event.preventDefault();
-            load_with_pjax(trade_url);
-        }).addClass('unbind_later');
-
-    }
-};
-
 var Header = function(params) {
     this.user = params['user'];
     this.client = params['client'];
@@ -369,7 +233,6 @@ Header.prototype = {
         this.show_or_hide_login_form();
         this.register_dynamic_links();
         if (!this.clock_started) this.start_clock();
-        this.simulate_input_placeholder_for_ie();
     },
     on_unload: function() {
         this.menu.reset();
@@ -409,25 +272,6 @@ Header.prototype = {
             }
             $("#client_loginid").html(loginid_select);
         }
-    },
-    simulate_input_placeholder_for_ie: function() {
-        var test = document.createElement('input');
-        if ('placeholder' in test)
-            return;
-        $('input[placeholder]').each(function() {
-            var input = $(this);
-            $(input).val(input.attr('placeholder'));
-            $(input).focus(function() {
-                if (input.val() == input.attr('placeholder')) {
-                    input.val('');
-                }
-            });
-            $(input).blur(function() {
-                if (input.val() === '' || input.val() == input.attr('placeholder')) {
-                    input.val(input.attr('placeholder'));
-                }
-            });
-        });
     },
     register_dynamic_links: function() {
         var logged_in_url = page.url.url_for('');
--- src/javascript/binary/utility.js
+++ src/javascript/binary/utility.js
@@ -364,22 +364,64 @@ function initTabs() {
 }
 
 function initDateTimePicker() {
-    $('.has-date-picker').pickadate(); //.has-date-picker    input[type=date]
+    jQuery.extend( jQuery.fn.pickadate.defaults, {
+        monthsFull: [
+            text.localize('January'),
+            text.localize('February'),
+            text.localize('March'),
+            text.localize('April'),
+            text.localize('May'),
+            text.localize('June'),
+            text.localize('July'),
+            text.localize('August'),
+            text.localize('September'),
+            text.localize('October'),
+            text.localize('November'),
+            text.localize('December')
+        ],
+        monthsShort: [
+            text.localize('Jan'),
+            text.localize('Feb'),
+            text.localize('Mar'),
+            text.localize('Apr'),
+            text.localize('May'),
+            text.localize('Jun'),
+            text.localize('Jul'),
+            text.localize('Aug'),
+            text.localize('Sep'),
+            text.localize('Oct'),
+            text.localize('Nov'),
+            text.localize('Dec')
+        ],
+        weekdaysFull: [
+            text.localize('Sunday'),
+            text.localize('Moonday'),
+            text.localize('Tuesday'),
+            text.localize('Wednesday'),
+            text.localize('Thursday'),
+            text.localize('Friday'),
+            text.localize('Saturday')
+        ],
+        weekdaysShort: [
+            text.localize('Su'),
+            text.localize('Mo'),
+            text.localize('Tu'),
+            text.localize('We'),
+            text.localize('Th'),
+            text.localize('Fr'),
+            text.localize('Sa')
+        ],
+        today: text.localize('Today'),
+        clear: text.localize('Clear'),
+        firstDay: 1,
+        format: 'd mmmm yyyy г.',
+        formatSubmit: 'yyyy/mm/dd'
+    });
+
+    jQuery.extend( jQuery.fn.pickatime.defaults, {
+        clear: text.localize('Clear'),
+    });
+
+    $('input[type=date]').pickadate(); // .has-date-picker
+    $('input[type=time]').pickatime(); // .has-time-picker
 }
-
-/*
-    var options = {
-        dateFormat: 'yy-mm-dd',
-        maxDate: next_year,
-    };
-
-        var opts = {
-            timeSeparator: ':',
-            showLeadingZero: true,
-            howMinutesLeadingZero: true,
-            hourText: text.localize("Hour"),
-            minuteText: text.localize("Minute"),
-            minTime: {},
-            maxTime: {},
-        };
-*/
